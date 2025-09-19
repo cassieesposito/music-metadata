f6cf1e2b84cea7b17549f32ca27de4f91afb66f6
Cassie Esposito <dev@timevault.org>
2025-09-18 20:43:25 -0700

    Switched from requests to aiohttp in order to parallelize http calls Switched from flask to aiohttp since I'm using aiohttp anyway and it was a good learning excercise Refactored from a procedural model to object oriented model because the code was starting to grow to the size where spaghetti is possible. Blame scope creep

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # music-metadata
+This webapp takes Deezer album or track IDs as input and returns the data necessary to add a release to the MusicBrainz database
+
 ##Execution
 Run this however you run flask apps. Outside of production environments you can use:
 
@@ -12,7 +14,9 @@ Do not do NOT do this in any sort of production environment, it will allow any u
 - A UI that isn't ugly as sin (I don't really have these skills. Someone help?)
 - More search options?
 - Make enter key search
-- Parallelize requests for massive performance improvement
-- Select from options when multiple results are returned by soundexchange
+- Handle cases where soundexchange returns multiple results for a single isrc. This will have to wait until I find such a case. I've found them before, but I've forgotten which ones
+  - Select from options when multiple results are returned by soundexchange?
 - CLI?
-- Push data to MusicBrainz
+- Push data directly to MusicBrainz?
+- Identify albums using other input methods (spotify metadata? isrc?)
+
--- music-metadata.py
+++ music-metadata.py
@@ -1,111 +1,225 @@
 #!/usr/bin/python3
 
-DEEZER_API = 'https://api.deezer.com'
-SX_API='https://isrc-api.soundexchange.com/api/ext'
+# Reference Deezer IDs
+# Album 302309: The Highwaymen - The Road goes on Forever
+# Track 30644201: Afroman - I feel Good from Waiting to Inhale
+# Album 592830302: John Kirby - The Classics Sampler (missing ISRCs)
 
-from flask import Flask, jsonify, render_template, request
-
-import requests
+import asyncio
+from aiohttp import web
+import aiohttp
 
 from pprint import pp as pprint
 
+DEEZER_API = 'https://api.deezer.com'
+SX_API='https://isrc-api.soundexchange.com/api/ext'
+SX_RESULTS=100
+
+###########
+#  Setup  #
+###########
+def app():
+    app = web.Application()
+    app.add_routes([
+        web.get('/', index),
+        web.static('/', 'static'),
+        # web.get('/artist', getArtistData),
+        web.get('/album', getAlbumData),
+        web.get('/track', getAlbumDataFromTrack)
+    ])
+    app.on_startup.append(onStartup)
+    app.on_cleanup.append(onCleanup)
+    return app
+
+async def onStartup(app):
+    app['session'] = aiohttp.ClientSession()
+
+async def onCleanup(app):
+    await app['session'].close()
 
 
-deezerData = Flask(__name__,static_url_path='')
-
-@deezerData.route('/')
-def index():
-    return render_template('index.html')
-
-@deezerData.route('/artist')
-def getArtistData():
-    artistID=request.args.get('deezerID')
-    artist = requests.get(url=f'{DEEZER_API}/artist/{artistID}').json()
-    output = {'id':artistID,
-               'name': artist['name']}
-    return jsonify(output)
-
-@deezerData.route('/album')
-def getAlbumData():
-    return formAlbumOutput(request.args.get('deezerID'))
-
-@deezerData.route('/track')
-def getAlbumDataFromTrack():
-    trackID=request.args.get('deezerID')
-    track = requests.get(url=f'{DEEZER_API}/track/{trackID}').json()
-    return formAlbumOutput(track['album']['id'])
+############
+#  Routes  #
+############
+async def index(req):
+    return web.FileResponse('static/index.html')
 
 
+# async def getArtistData(req):
+#     session = req.app['session']
+#     artistID = req.query['deezerID']
+    
+#     async with session.get(url=f'{DEEZER_API}/artist/{artistID}') as res:
+#         artist = await res.json()
+    
+#     output = {'id':artistID,
+#             'name': artist['name'],
+#     }
 
-def formAlbumOutput(deezerID):
-    dzData = requests.get(url=f'{DEEZER_API}/album/{deezerID}').json()
-
-    sxSession = soundexchangeSession()
-    sxData = sxSession.post(f'{SX_API}/recordings', json={  "searchFields": {"icpn": dzData['upc']},
-                                                            "start": 0,
-                                                            "number": 100,
-                                                            "showReleases": True
-                                                            }).json()
-
-    tracks = buildTracks(dzData,sxData, sxSession)
-
-    output = {'id':deezerID,
-               'title': dzData['title'],
-               'artist': dzData['artist']['name'],
-               'deezerReleaseDate': dzData['release_date'],
-               'upc': dzData['upc'],
-               'label': dzData['label'],
-               'tracks': tracks,
-               'albumLink': dzData['link'],
-               'artistLink': f'https://www.deezer.com/en/artist/{dzData["artist"]["id"]}'
-               }
-    return jsonify(output)
+#     return web.json_response(output)
 
 
-def soundexchangeSession():
-    session = requests.session()
-    session.get(f'{SX_API}/login')
-    return session
+async def getAlbumData(req):
+    session = req.app['session'] 
+    albumID = req.query['deezerID']
 
-def buildTracks(dzData,sxData,sxSession):
-    tracks = []
+    album = Album()
+    await album.albumInit(session,albumID)
+    pprint (vars(album))
 
-    for record in dzData['tracks']['data']:
-        track = {'dz' : {}, 'sx' : {}}
-        dzTrack = requests.get(url=f'{DEEZER_API}/track/{record['id']}').json()
-        sxTracks = [track for track in sxData['recordings'] if track['isrc'] == dzTrack['isrc']]
-        if not sxTracks:
-            sxRes = sxSession.post (f'{SX_API}/recordings/', json={ "searchFields": {"isrc":dzTrack['isrc']},
-                                                                    "start": 0,
-                                                                    "number": 100,
-                                                                    "showReleases": True
-                                                                    }).json()
-            if sxRes['numberOfRecordings']:
-                sxTracks = sxRes['recordings']
-
-        track['dz'] |= {    'title': dzTrack['title'],
-                            'track': dzTrack['track_position'],
-                            'disk': dzTrack['disk_number'],
-                            'artist': dzTrack['artist']['name'],
-                            'isrc': dzTrack['isrc'],
-                            'artists': list(map(lambda artist: artist['name'], dzData['contributors'])),
-                            'length': f'{int(dzTrack['duration'] / 60)}:{dzTrack['duration'] % 60}',
-                            'date': dzTrack['release_date']
-                            }
-        if sxTracks:      
-            track['sx'] |= {'title': [ e.get('recordingTitle') for e in sxTracks ],
-                            'length': [ e.get('duration') for e in sxTracks ],
-                            'version': [ e.get('recordingVersion') for e in sxTracks ],
-                            'isrcValid': [ e.get('isValidIsrc') for e in sxTracks ],
-                            'year': [ e.get('recordingYear') for e in sxTracks ],
-                            'label': [ e.get('releaseLabel') for e in sxTracks ],
-                            'date': [ e.get('releaseDate') for e in sxTracks ]
-                            }
-        tracks.append(track)
-            
-    return tracks
+    return web.json_response(vars(album))
 
 
+async def getAlbumDataFromTrack(req):
+    session = req.app['session'] 
+    trackID = req.query['deezerID']
 
-if __name__ == '__main__':
-    deezerData.run(debug=True)
+    track = await get(session, f'{DEEZER_API}/track/{trackID}')
+    albumID = track['album']['id']
+
+    album = Album()
+    await album.albumInit(session,albumID)
+    print (vars(album))
+
+    return web.json_response(vars(album))
+
+
+#####################
+#  Data Processing  #
+#####################
+class Album:
+    def __init__(self):
+        self.deezerId = None
+        self.title = None
+        self.artist = None
+        self.deezerReleaseDate = None
+        self.upc = None
+        self.label = None
+        self.albumLink = None
+        self.artistLink = None
+        self.tracks = []
+
+
+    async def albumInit(self, session, albumID):
+        data = {'dzAlbum': {}, 'sxAlbum': {}, 'dzTracks': [], 'sxTracks': {}}
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(self._sxLogin(session))
+            res = tg.create_task(self._getDzAlbum(session, albumID))
+        data['dzAlbum'] = res.result()
+
+        async with asyncio.TaskGroup() as tg:
+            data['dzTracks'] = tg.create_task(self._getDzTracks(session, data))
+            data['sxAlbum'] = tg.create_task(self._getSxAlbum(session,data))
+        data['dzTracks'] = await data['dzTracks']
+        data['sxAlbum'] = await data['sxAlbum']
+
+        data['sxTracks'] = await self._getSxTracks(session,data)
+
+        self._populateVars(data)
+
+
+    async def _sxLogin(self, session):
+        return await get(session, f'{SX_API}/login')
+
+
+    async def _getDzAlbum (self, session, albumID):
+        dzAlbum = await get(session,f'{DEEZER_API}/album/{albumID}')
+        return dzAlbum
+
+
+    async def _getDzTracks (self, session, data):
+        trackIDs = list(map(lambda data: data['id'], data['dzAlbum']['tracks']['data']))
+        async with asyncio.TaskGroup() as tg:
+            res = [tg.create_task(get(session,f'{DEEZER_API}/track/{id}')) for id in trackIDs]
+        dzTracks = await asyncio.gather(*res)
+        return dzTracks
+
+
+    async def _getSxAlbum (self, session, data):
+        payload = {
+            "searchFields": {"icpn": data['dzAlbum']['upc']},
+            "start": 0,
+            "number": SX_RESULTS,
+            "showReleases": True
+        }
+        return await post(session, f'{SX_API}/recordings', payload)
+
+
+    async def _getSxTracks (self, session, data):
+        # Create dict: sxTracks with the format {isrc: [sxTrackData]}. Populate it with data from data['sxAlbum']
+        sxTracks = {}
+        for dzTrack in data['dzTracks']:
+            sxTracks[dzTrack['isrc']] = [sxTrack for sxTrack in data['sxAlbum']['recordings']
+                                         if sxTrack['isrc'] == dzTrack['isrc']]
+
+        # Query soundexchange for information about any tracks that were not included in the album
+        # query and populate sxTracks['isrc'] with the results
+        missingISRCs = [track['isrc'] for track in data['dzTracks'] if not sxTracks[track['isrc']]]
+        missingSxTracks = []
+        payload = {"searchFields": {"isrc": None}, "start": 0, "number": SX_RESULTS, "showReleases": True}
+        async with asyncio.TaskGroup() as tg:
+            for isrc in missingISRCs:
+                payload['searchFields']['isrc'] = isrc
+                missingSxTracks.append(asyncio.create_task(post(session,f'{SX_API}/recordings/',payload)))
+        missingSxTracks = await asyncio.gather(*missingSxTracks)
+        for track in missingSxTracks:
+            print (track)
+            print ("-----------------END MISSING TRACK-----------------")
+
+        return sxTracks
+
+
+    def _populateVars(self, data):
+        self.deezerId = data['dzAlbum']['id']
+        self.title = data['dzAlbum']['title']
+        self.artist = data['dzAlbum']['artist']['name']
+        self.deezerReleaseDate = data['dzAlbum']['release_date']
+        self.upc = data['dzAlbum']['upc']
+        self.label = data['dzAlbum']['label']
+        self.albumLink = data['dzAlbum']['link']
+        self.artistLink = f'https://www.deezer.com/artist/{data['dzAlbum']['artist']['id']}'
+        for track in data['dzTracks']:
+            self.tracks.append(self._buildTrack(track, data))
+        return
+
+
+    def _buildTrack (self, dzTrack, data):
+        isrc = dzTrack['isrc']
+        output = {'dz' : {}, 'sx' : {}}
+        output['dz'] = {
+            'title': dzTrack['title'],
+            'track': dzTrack['track_position'],
+            'disk': dzTrack['disk_number'],
+            'artist': dzTrack['artist']['name'],
+            'isrc': isrc,
+            'artists': list(map(lambda artist: artist['name'], dzTrack['contributors'])),
+            'length': f'{int(dzTrack['duration'] / 60)}:{str(dzTrack['duration'] % 60).zfill(2)}',
+            'date': dzTrack['release_date']
+            }
+        output['sx'] = {
+            'title': [ e.get('recordingTitle') for e in data['sxTracks'][isrc] ],
+            'length': [ e.get('duration') for e in data['sxTracks'][isrc] ],
+            'version': [ e.get('recordingVersion') for e in data['sxTracks'][isrc] ],
+            'isrcValid': [ e.get('isValidIsrc') for e in data['sxTracks'][isrc] ],
+            'year': [ e.get('recordingYear') for e in data['sxTracks'][isrc] ],
+            'label': [ e.get('releaseLabel') for e in data['sxTracks'][isrc] ],
+            'date': [ e.get('releaseDate') for e in data['sxTracks'][isrc] ]
+            }
+        return output
+
+
+##################
+#  HTTP Methods  #
+##################
+async def get(session, url, params=dict()):
+    async with session.get(url, params=params) as res:
+        return await res.json()
+
+
+async def post(session, url, payload):
+    async with session.post(url, json=payload) as res:
+        return await res.json()
+
+
+if __name__ == "__main__":
+    web.run_app(app(), port=5000)
